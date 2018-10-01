de2c8304f317be5f3a6b7dc7755e1e2d93f9e2ea
Julia <42332811+jmanning21@users.noreply.github.com>
2018-10-01 21:40:05 +1000

    Update requestedstationery.sql

--- DBFiles/MSSQL/requestedstationery.sql
+++ DBFiles/MSSQL/requestedstationery.sql
@@ -1,14 +1,14 @@
-USE [sepdb]
+USE sepdb
 GO
 
-ALTER TABLE [dbo].[requestedstationery] DROP CONSTRAINT [reqstatFKstationeryrequest]
+ALTER TABLE dbo.requestedstationery DROP CONSTRAINT reqstatFKstationeryrequest
 GO
 
-ALTER TABLE [dbo].[requestedstationery] DROP CONSTRAINT [reqstatFKproduct]
+ALTER TABLE dbo.requestedstationery DROP CONSTRAINT reqstatFKproduct
 GO
 
 /****** Object:  Table [dbo].[requestedstationery]    Script Date: 3/09/2018 1:53:20 AM ******/
-DROP TABLE [dbo].[requestedstationery]
+DROP TABLE dbo.requestedstationery
 GO
 
 /****** Object:  Table [dbo].[requestedstationery]    Script Date: 3/09/2018 1:53:20 AM ******/
@@ -18,34 +18,27 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE TABLE [dbo].[requestedstationery](
-	[requestID] [int] NOT NULL,
-	[productID] [int] NOT NULL,
-	[quantity] [int] NOT NULL,
+CREATE TABLE dbo.requestedstationery(
+	requestID int NOT NULL,
+	productID int NOT NULL,
+	quantity int NOT NULL,
 PRIMARY KEY CLUSTERED 
-(
-	[requestID] ASC,
-	[productID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY]
+(requestID ASC, productID ASC)) ON PRIMARY
 GO
 
-ALTER TABLE [dbo].[requestedstationery]  WITH CHECK ADD  CONSTRAINT [reqstatFKproduct] FOREIGN KEY([productID])
-REFERENCES [dbo].[product] ([productID])
+ALTER TABLE dbo.requestedstationery  WITH CHECK ADD  CONSTRAINT reqstatFKproduct FOREIGN KEY(productID)
+REFERENCES dbo.product (productID)
 GO
 
-ALTER TABLE [dbo].[requestedstationery] CHECK CONSTRAINT [reqstatFKproduct]
+ALTER TABLE dbo.requestedstationery CHECK CONSTRAINT reqstatFKproduct
 GO
 
-ALTER TABLE [dbo].[requestedstationery]  WITH CHECK ADD  CONSTRAINT [reqstatFKstationeryrequest] FOREIGN KEY([requestID])
-REFERENCES [dbo].[stationeryrequest] ([requestID])
+ALTER TABLE dbo.requestedstationery  WITH CHECK ADD  CONSTRAINT reqstatFKstationeryrequest FOREIGN KEY(requestID)
+REFERENCES dbo.stationeryrequest (requestID)
 GO
 
-ALTER TABLE [dbo].[requestedstationery] CHECK CONSTRAINT [reqstatFKstationeryrequest]
+ALTER TABLE dbo.requestedstationery CHECK CONSTRAINT reqstatFKstationeryrequest
 GO
-
-
-
 
 INSERT INTO requestedstationery VALUES (1,3,10),(2,4,23),(3,5,16);
 GO
