543e1637e90a0d4f87e59f0f0ef675787129c8f5
Julia <42332811+jmanning21@users.noreply.github.com>
2018-10-01 21:36:42 +1000

    Update product.sql

--- DBFiles/MSSQL/product.sql
+++ DBFiles/MSSQL/product.sql
@@ -1,11 +1,8 @@
-USE [sepdb]
-GO
-
-ALTER TABLE [dbo].[product] DROP CONSTRAINT [productFKsupplier]
+USE sepdb
 GO
 
 /****** Object:  Table [dbo].[product]    Script Date: 3/09/2018 1:46:14 AM ******/
-DROP TABLE [dbo].[product]
+DROP TABLE dbo.product
 GO
 
 /****** Object:  Table [dbo].[product]    Script Date: 3/09/2018 1:46:14 AM ******/
@@ -15,29 +12,23 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE TABLE [dbo].[product](
-	[productID] [int] NOT NULL,
-	[supplierID] [int] NOT NULL,
-	[name] [varchar](20) NOT NULL,
-	[description] [varchar](100) NOT NULL,
-	[stock] [int] NOT NULL,
-	[price] [decimal](18, 0) NOT NULL,
+CREATE TABLE dbo.product(
+	productID [int NOT NULL,
+	supplierID [int NOT NULL,
+	name varchar(20) NOT NULL,
+	description [varchar](100) NOT NULL,
+	stock int NOT NULL,
+	price decimal(18, 0) NOT NULL,
 PRIMARY KEY CLUSTERED 
-(
-	[productID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY]
+(productID ASC)) 
 GO
 
-ALTER TABLE [dbo].[product]  WITH CHECK ADD  CONSTRAINT [productFKsupplier] FOREIGN KEY([supplierID])
-REFERENCES [dbo].[supplier] ([supplierID])
+ALTER TABLE dbo.product  WITH CHECK ADD  CONSTRAINT productFKsupplier FOREIGN KEY(supplierID)
+REFERENCES dbo.supplier (supplierID)
 GO
 
-ALTER TABLE [dbo].[product] CHECK CONSTRAINT [productFKsupplier]
+ALTER TABLE dbo.product CHECK CONSTRAINT productFKsupplier
 GO
 
-
-
-
-INSERT INTO product VALUES (1,1,'blue pen','blue ballpoint pen',50,3),(2,1,'black pen','black ballpoint pen',50,3),(3,2,'butchers paper','A3 butchers paper',50,5),(4,1,'whiteboard marker','black whiteboard marker',50,4),(5,3,'ruler','clear plastic 30cm ruler',50,3);
+INSERT INTO product VALUES (1,1,'blue pen','A blue ballpoint pen that glides along the paper',50,3),(2,1,'black pen','A black ballpoint pen that is perfect for examinations',50,3),(3,2,'butchers paper','A pack of three A3 butchers paper sheets',0,5),(4,1,'whiteboard marker','A black whiteboard marker that has a thick point and does not stain',50,4),(5,3,'ruler','A clear plastic 30cm ruler accepted by examinors worldwide',50,3);
 GO
