81af6f183e7442dc50494046fd73564ba484d985
Julia <42332811+jmanning21@users.noreply.github.com>
2018-10-04 23:44:57 +1000

    Update stationeryrequest.sql

--- DBFiles/MSSQL/stationeryrequest.sql
+++ DBFiles/MSSQL/stationeryrequest.sql
@@ -19,9 +19,7 @@ SET QUOTED_IDENTIFIER ON
 GO
 
 CREATE TABLE dbo.stationeryrequest(
-	DECLARE @requestID uniqueidentifier
-	SET @requestID = NEWID(),
-	/*requestID int NOT NULL,*/
+	requestID int IDENTITY NOT NULL,
 	userID int NOT NULL,
 	productID int NOT NULL,
 	requestDate date NOT NULL,
