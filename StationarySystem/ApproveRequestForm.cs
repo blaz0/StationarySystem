deba0ebbebfad5c79e1383fa7a93d5e305faff44
mkoh <34255017+blaz0@users.noreply.github.com>
2018-10-06 16:46:30 +1000

    UC 302

---
+++ StationarySystem/ApproveRequestForm.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StationarySystem
+{
+    public partial class ApproveRequestForm : Form
+    {
+        public ApproveRequestForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ProductsLbl_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cancel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OnMouseEnter(object sender, EventArgs e)
+        {
+            cancel.Font = new Font(cancel.Font, FontStyle.Underline);
+        }
+
+        private void OnMouseLeave(object sender, EventArgs e)
+        {
+            cancel.Font = new Font(cancel.Font, FontStyle.Regular);
+        }
+    }
+}
