f29fb2a33e4eb29b415dc21b00ff488efeef48f9
mkoh <34255017+blaz0@users.noreply.github.com>
2018-10-08 15:24:20 +1100

    added travis and testing

---
+++ StationerySystemTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StationerySystemTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
